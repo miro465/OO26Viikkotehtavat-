48c89b8e0fa8ccfc1f63269d205a31625f995856
Miro <miro.haapasalo03@gmail.com>
2026-02-22 22:14:43 +0200

    viikko7

---
+++ OOviikko7/main.cpp
@@ -0,0 +1,9 @@
+#include "calculator.h"
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    Calculator calc;
+    calc.show();
+    return a.exec();
+}
